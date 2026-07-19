6d207e12288fcb5379f6fab07b050c444e118622
JOTHI <frost.lily2k4@gmail.com>
2026-07-19 16:27:46 +0530

    Refactor script.js to add CSS styles and remove function
    Removed the showMessage function and added CSS styles for body, card, images, headings, paragraphs, progress bars, blockquotes, and buttons.

--- script.js
+++ script.js
@@ -1,4 +1,96 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-    "Luffy inspires me with his courage, determination, loyalty, and never-give-up attitude!";
+body{
+
+    margin:0;
+    font-family:Arial, Helvetica, sans-serif;
+
+    background:linear-gradient(to right,#ff9966,#ff5e62);
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100vh;
+}
+
+.card{
+
+    background:white;
+    width:380px;
+    padding:25px;
+    border-radius:20px;
+    text-align:center;
+
+    box-shadow:0px 10px 25px rgba(0,0,0,.3);
+
+}
+
+img{
+
+    width:180px;
+    height:220px;
+    object-fit:cover;
+
+}
+
+h1{
+
+    color:#ff4500;
+
+}
+
+h3{
+
+    color:#444;
+
+}
+
+p{
+
+    text-align:left;
+
+}
+
+progress{
+
+    width:100%;
+    height:20px;
+
+}
+
+blockquote{
+
+    background:#fff3cd;
+    padding:10px;
+    border-left:5px solid orange;
+    border-radius:5px;
+
+}
+
+button{
+
+    margin:8px;
+    padding:10px 20px;
+
+    border:none;
+    border-radius:8px;
+
+    cursor:pointer;
+
+    background:#ff4500;
+    color:white;
+    font-size:15px;
+
+}
+
+button:hover{
+
+    background:#e63e00;
+
+}
+
+#fact{
+
+    margin-top:15px;
+    font-weight:bold;
+    color:green;
+
 }
